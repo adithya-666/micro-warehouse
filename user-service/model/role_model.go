935ad3e3b7440fcc799300dd1af0d75945c355f3
adithya-666 <adithyaasus07@gmail.com>
2026-05-27 22:04:47 +0700

    Add file git ingnore

--- user-service/model/role_model.go
+++ user-service/model/role_model.go
@@ -1,1 +1,10 @@
 package model
+
+import "time"
+
+type Role struct {
+	ID        uint      `json:"id" gorm:"primaryKey;autoIncrement"`
+	Name      string    `json:"name" gorm:"unique;not null"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
